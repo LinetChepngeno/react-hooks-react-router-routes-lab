89b085a63faca4224e95e54620ca2ae1c036b552
Linet Chepngeno <linetmutai@gmail.com>
2024-02-17 18:05:58 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div>
+    <div className='navbar'>
       <NavLink exact to="/">Home</NavLink>
       <NavLink to="/movies">Movies</NavLink>
       <NavLink to="/directors">Directors</NavLink>
